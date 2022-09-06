125cab5cc6bead9c87b81375a47dbe61c7c86609
umutnl <96056187+umutnl@users.noreply.github.com>
2022-09-06 18:43:20 +0300

    first commit

--- src/About.jsx
+++ src/About.jsx
@@ -8,26 +8,21 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-400">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-400">
+            About
+          </p>
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          voluptate eius dolores id quae. Officia, nulla quaerat quis quam harum
-          saepe ipsa accusantium repellat quos pariatur veritatis velit deleniti
-          voluptatem alias ex, obcaecati ad. Maiores molestiae dolor odit ipsum
-          laboriosam. Enim natus illo earum veniam, incidunt doloremque
-          aspernatur odio provident?
+          A passionate programming freelancer witH experience in web app and
+          desktop application development.
         </p>
         <br />
 
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni amet
-          eum, unde quaerat ullam fugit deserunt esse adipisci quibusdam in
-          praesentium explicabo soluta veniam maxime! Quo possimus nemo corporis
-          debitis, aspernatur quia labore quas est non necessitatibus placeat,
-          culpa reiciendis libero, sunt iure. Laborum omnis aperiam molestias
-          debitis delectus atque?
+          I get energy from coming up with creative solutions. I have the
+          following skills html5 css3 tailwind css, react and wordpress. I'm
+          programming on a daily basis it's a passion for me
         </p>
       </div>
     </div>
